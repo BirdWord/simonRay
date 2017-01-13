7b10221cf15d8315960343aa403ed14d4a8ec5f1
Daniel2 <dgoon9608>
2017-01-12 21:16:28 -0500

    I think its good?

--- SimonRay/src/partnercode/Progress.java
+++ SimonRay/src/partnercode/Progress.java
@@ -11,24 +11,21 @@ import simonRay.ProgressInterfaceRay;
 
 public class Progress extends Component implements ProgressInterfaceRay {
 
-	private static final int WIDTH = 120;
-	private static final int HEIGHT = 50;
-
 	private boolean gameOver;
-	private String round;
-	private String sequence;
+	private int roundLevel;
+	private int sequenceSize;
 
 	public Progress() {
-		super(50,50,WIDTH,HEIGHT);
+		super(50,50,120,50);
 	}
 
 	public void setRound(int roundNumber) {
-		round = "Round "+roundNumber;
+		roundLevel = roundNumber;
 		update();
 	}
 
 	public void setSequenceSize(int size) {
-		sequence = "Sequence size "+size;
+		sequenceSize = size;
 		update();
 	}
 
@@ -45,22 +42,20 @@ public class Progress extends Component implements ProgressInterfaceRay {
 		FontMetrics fm = g.getFontMetrics();
 		if(gameOver){
 			g.setColor(new Color(255,55,90));
-			g.fillRect(0, 0, WIDTH, HEIGHT);
+			g.fillRect(0, 0, 120, 50);
 			g.setColor(Color.white);
-			String go = "GAME OVER!";
-			g.drawString(go, (WIDTH - fm.stringWidth(go))/2, 20);
-			g.drawString(sequence, (WIDTH - fm.stringWidth(sequence))/2, 40);
+			String go = "Game over!";
+			g.drawString(go, (120 - fm.stringWidth(go))/2, 20);
+			g.drawString("Sequence Size = " + sequenceSize, (120 - fm.stringWidth("Sequence Size = " + sequenceSize))/2, 40);
 
 		}else{
 			g.setColor(new Color(220,255,230));
-			g.fillRect(0, 0, WIDTH, HEIGHT);
+			g.fillRect(0, 0, 120, 50);
 			g.setColor(Color.black);
-			g.drawRect(0, 0, WIDTH-1, HEIGHT-1);
-			if(round !=null && sequence != null){
-
-				g.drawString(round, (WIDTH - fm.stringWidth(round))/2, 20);
-				g.drawString(sequence, (WIDTH - fm.stringWidth(sequence))/2, 40);
-			}
+			g.drawRect(0, 0, 120-1, 50-1);
+			g.drawString("Round Number = " + roundLevel, (120 - fm.stringWidth("Round Number = " + roundLevel))/2, 20);
+			g.drawString("Sequence Size = " + sequenceSize, (120 - fm.stringWidth("Sequence Size = " + sequenceSize))/2, 40);
+			
 		}
 	}
 
